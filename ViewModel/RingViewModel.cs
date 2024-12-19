5c7f5a0a6176e388bff31e40e12adf73d9ad1255
Adriano Silva de Santana <mano1die2mili@gmail.com>
2024-12-18 23:23:14 -0300

    Adjusts

---
+++ ViewModel/RingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Power.Domain.RingsAggregate;
+
+namespace Power.ViewModel
+{
+    public class RingViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Power { get; set; }
+        public string Carrier { get; set; }
+        public ForgedBy ForgedBy { get; set; }
+        public string Image { get; set; }
+    }
+}
