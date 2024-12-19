353b7a7a55f381807bd906a53fbccaf2d189b55c
Adriano Silva de Santana <mano1die2mili@gmail.com>
2024-12-18 23:01:35 -0300

    Adjusts in layout

--- Pages/Ring/Edit.cshtml.cs
+++ Pages/Ring/Edit.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Power.Domain.RingsAggregate;
 using Power.Domain.RingsAggregate.Services;
 
 namespace Power.Pages.Ring
@@ -7,6 +9,8 @@ namespace Power.Pages.Ring
     public class EditModel : PageModel
     {
         private readonly IRingService _service;
+
+        public List<SelectListItem> RingPower { get; private set; } = new List<SelectListItem>();
 
         public EditModel(IRingService service)
         {
@@ -30,6 +34,14 @@ namespace Power.Pages.Ring
                 return NotFound();
             }
 
+            ViewData["ForgedByList"] = new SelectList(Enum.GetValues(typeof(ForgedBy)));
+
+            return Page();
+        }
+
+        public IActionResult GetListSelect()
+        {
+            RingPower = GetListOfRingPowers();
             return Page();
         }
 
@@ -43,6 +55,18 @@ namespace Power.Pages.Ring
             await _service.Update(Ring.Id, Ring.Name, Ring.Power, Ring.Carrier, Ring.ForgedBy, Ring.Image);
 
             return RedirectToPage("./Index");
+        }
+
+        private List<SelectListItem> GetListOfRingPowers()
+        {
+            return Enum.GetValues(typeof(ForgedBy))
+                .Cast<ForgedBy>()
+                .Select(x => new SelectListItem()
+                {
+                    Value = ((int)x).ToString(),
+                    Text = x.ToString()
+                })
+                .ToList();
         }
 
         private bool RingExists(Guid id)
